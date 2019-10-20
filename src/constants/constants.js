0d166cc4cebeefccdf4fec1ddfc94da4747e53a0
Swapnil <swapnilre@gmail.com>
2019-10-20 20:08:53 +0530

    First Inital Commit

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+const PINCODE_CDN_URL =
+  "https://res.cloudinary.com/duxzkrsmd/raw/upload/v1571489048/pincode_ua1m9w.json";
+
+const LOCALITY_CDN_URL =
+  "https://res.cloudinary.com/duxzkrsmd/raw/upload/v1571489027/locality_vuzxcl.json";
+const INCOME_CDN_URL =
+  "https://res.cloudinary.com/duxzkrsmd/raw/upload/v1571488997/income_nnmigg.json";
+const EXPENDITURE_CDN_URL =
+  "https://res.cloudinary.com/duxzkrsmd/raw/upload/v1571488997/expenditure_hmevng.json";
+
+export default {
+  PINCODE_CDN_URL,
+  LOCALITY_CDN_URL,
+  INCOME_CDN_URL,
+  EXPENDITURE_CDN_URL
+};
---
+++ src/service-worker.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+var CACHE = "network-or-cache";
+
+self.addEventListener("install", function(evt) {
+  console.log("The service worker is being installed.");
+  evt.waitUntil(precache());
+});
+
+self.addEventListener("fetch", function(evt) {
+  //   console.log("The service worker is serving the asset.");
+  evt.respondWith(
+    fromNetwork(evt.request, 400).catch(function() {
+      return fromCache(evt.request);
+    })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.addAll(["./controlled.html", "./asset"]);
+  });
+}
+
+function fromNetwork(request, timeout) {
+  return new Promise(function(fulfill, reject) {
+    var timeoutId = setTimeout(reject, timeout);
+
+    fetch(request).then(function(response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+    }, reject);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      return matching || Promise.reject("no-match");
+    });
+  });
+}
